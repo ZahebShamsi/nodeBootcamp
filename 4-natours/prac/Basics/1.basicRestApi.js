b45aba5a6182860bdfc9fc5bf1646e4d45c788f4
zaheb <zaheb@policybazaar.com>
2023-07-23 00:38:14 +0530

    uncomment code

--- 4-natours/prac/Basics/1.basicRestApi.js
+++ 4-natours/prac/Basics/1.basicRestApi.js
@@ -12,14 +12,14 @@ const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simpl
 
 //#region - 1. BASIC ROUTING
     // a) Send Text
-    // app.get('/', (req, res) => {
-    //     res.status(200).send('Hello From Server Side');
-    // })
+    app.get('/', (req, res) => {
+        res.status(200).send('Hello From Server Side');
+    })
 
     // b) Send JSON Obj - automatically appends "Content-Type: application/json; charset=utf-8" to response header
-    // app.get('/', (req, res) => {
-    //     res.status(200).json({'message':'Hello From Server Side', 'app': 'Natours'});
-    // })
+    app.get('/', (req, res) => {
+        res.status(200).json({'message':'Hello From Server Side', 'app': 'Natours'});
+    })
 
     // c) if you dont have method defined then server will send back below template
     {/* <body>
@@ -27,37 +27,37 @@ const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simpl
     </body> */}
 
     // d) post method...
-    // app.post('/', (req, res) => {
-    //     res.status(200).json({'message':'You can use post method', 'app': 'Natours'});
-    // })
+    app.post('/', (req, res) => {
+        res.status(200).json({'message':'You can use post method', 'app': 'Natours'});
+    })
 //#endregion
 
 //#region - 2. HANDLING GET REQUEST
 
-    // app.get('/api/v1/tours', (req,res) => {
-    //     res.status(200).json({
-    //         'status' : 'success',
-    //         result : tours.length,
-    //         data : { tours }
-    //     })
-    // })
+    app.get('/api/v1/tours', (req,res) => {
+        res.status(200).json({
+            'status' : 'success',
+            result : tours.length,
+            data : { tours }
+        })
+    })
 
 //#endregion
 
 //#region - 3. HANDLING POST REQUEST
-    // app.post('/api/v1/tours', (req,res) => {
+    app.post('/api/v1/tours', (req,res) => {
 
-    //     const newId =  tours[tours.length - 1].id + 1;
-    //     const newTour = Object.assign({id: newId}, req.body);
-    //     tours.push(newTour);
+        const newId =  tours[tours.length - 1].id + 1;
+        const newTour = Object.assign({id: newId}, req.body);
+        tours.push(newTour);
 
-    //     fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err =>{
-    //         res.status(201).json({
-    //             'status' : 'success',
-    //             data : { tour : newTour }
-    //         })
-    //     });
-    // })
+        fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err =>{
+            res.status(201).json({
+                'status' : 'success',
+                data : { tour : newTour }
+            })
+        });
+    })
 //#endregion
 
 //#region - 4. HANDLING URL PARAMS
@@ -66,63 +66,63 @@ const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simpl
     //  optional params => /api/v1/tours/:id/:x/:y? => { id: '2', x: '3', y: undefined }
 
     
-    // app.get('/api/v1/tours/:id', (req,res) => {
-    //     console.log(req.params);
-    //     const id = req.params.id * 1;
-    //     const tour = tours.find( el => el.id === id);
+    app.get('/api/v1/tours/:id', (req,res) => {
+        console.log(req.params);
+        const id = req.params.id * 1;
+        const tour = tours.find( el => el.id === id);
 
-    //     if(id > tours.length || !tour){
-    //         return res.status(404).json({
-    //             'status' : 'fail',
-    //             message : 'invalid Id'
-    //         })
-    //     }
+        if(id > tours.length || !tour){
+            return res.status(404).json({
+                'status' : 'fail',
+                message : 'invalid Id'
+            })
+        }
 
-    //     console.log(tour);
-    //     res.status(200).json({
-    //         'status' : 'success',
-    //         data : { tour }
-    //     })
-    // })
+        console.log(tour);
+        res.status(200).json({
+            'status' : 'success',
+            data : { tour }
+        })
+    })
 
 //#endregion
 
 //#region - 5. PATCH - only update the property and not all object
     
-    // app.patch('/api/v1/tours/:id', (req,res) => {
+    app.patch('/api/v1/tours/:id', (req,res) => {
 
-    //     const tour = tours.find( el => el.id === req.params.id);
+        const tour = tours.find( el => el.id === req.params.id);
 
-    //     if(req.params.id * 1 > tours.length){
-    //         return res.status(404).json({
-    //             'status' : 'fail',
-    //             message : 'invalid Id'
-    //         })
-    //     }
+        if(req.params.id * 1 > tours.length){
+            return res.status(404).json({
+                'status' : 'fail',
+                message : 'invalid Id'
+            })
+        }
 
-    //     res.status(200).json({
-    //         'status' : 'success',
-    //         data : '<>updated tour</>'
-    //     })
-    // })
+        res.status(200).json({
+            'status' : 'success',
+            data : '<>updated tour</>'
+        })
+    })
 
 //#endregion
 
 //#region - 6. DELETE 
     
-    // app.delete('/api/v1/tours/:id', (req,res) => {
-    //     const tour = tours.find( el => el.id === req.params.id);
-    //     if(req.params.id * 1 > tours.length){
-    //         return res.status(404).json({
-    //             'status' : 'fail',
-    //             message : 'invalid Id'
-    //         })
-    //     }
-    //     res.status(204).json({
-    //         'status' : 'success',
-    //         data : null
-    //     })
-    // })
+    app.delete('/api/v1/tours/:id', (req,res) => {
+        const tour = tours.find( el => el.id === req.params.id);
+        if(req.params.id * 1 > tours.length){
+            return res.status(404).json({
+                'status' : 'fail',
+                message : 'invalid Id'
+            })
+        }
+        res.status(204).json({
+            'status' : 'success',
+            data : null
+        })
+    })
 
 //#endregion
 
